1e3d46fe10671c14db7fd87cf698869932368036
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-03-12 12:58:22 -0700

    initiated and passed all tests

--- index.js
+++ index.js
@@ -2,47 +2,42 @@ const inquirer = require('inquirer');
 const { run } = require('jest-cli');
 const Shapes = require('./lib/shapes.js');
 const { writeFile } = require('fs/promises');
-const userLogo = [];
 
-const runShapes = new Shapes(initials, textColor, shapeColor);
+
 
 inquirer  // CLI
-    .prompt([  // accepts 
-        {
-            type: 'input',  // 1) 3 characters *TODO(confirm there are 3 character)
-            name: 'initials',
-            message: 'Input your initials.',
-        },
-        {
-            type: 'input',
-            name: 'textColor',  // 2) text color (keyword or hex value) *TODO(handle text or hex?)
-            message: 'Enter a text color.',
-        },
-        {
-            type: 'list',
-            name: 'shape',  // 3) prompt shape - list: circle, triangle, square  *TODO(when to if-else?)
-            message: 'Choose a shape.',
-            choices: ['Circle', 'Triangle', 'Square'],
-        },
-        {
-            type: 'input',
-            name: 'shapeColor',     // 4) shapes color (keyword or hex value)
-            message: 'Choose a background color.',
-        },
+.prompt([  // accepts 
+{
+    type: 'input',  // 1) 3 characters *TODO(confirm there are 3 character)
+    name: 'initials',
+    message: 'Input your initials.',
+},
+{
+    type: 'input',
+    name: 'textColor',  // 2) text color (keyword or hex value) *TODO(handle text or hex?)
+    message: 'Enter a text color.',
+},
+{
+    type: 'list',
+    name: 'shape',  // 3) prompt shape - list: circle, triangle, square  *TODO(when to if-else?)
+    message: 'Choose a shape.',
+    choices: ['Circle', 'Triangle', 'Square'],
+},
+{
+    type: 'input',
+    name: 'shapeColor',     // 4) shapes color (keyword or hex value)
+    message: 'Choose a background color.',
+},
 ]).then( ({ initials, textColor, shape, shapeColor} ) => {
     this.initials = initials;
     this.textColor = textColor;
+    this.shape = shape;
     this.shapeColor = shapeColor
-
-    if(shape === 'Circle') {
-        userLogo = runShapes.circle();
-    } else if(shape === 'Triangle') {
-        userLogo = runShapes.triangle();
-    }   else {
-        userLogo = runShapes.square();
-    };
+    runShapes(initials, textColor, shape, shapeColor);
+    
     createLogo(); 
 });
+const runShapes = new Shapes();
 
 const createLogo = () => {
     return writeFile('./examples', userLogo, (err) =>
