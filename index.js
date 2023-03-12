9952c2986733e3763c92f2f7aba63f5ec26cec85
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-03-12 15:51:36 -0700

    funtioning create svg

--- index.js
+++ index.js
@@ -1,49 +1,60 @@
 const inquirer = require('inquirer');
-const { run } = require('jest-cli');
 const Shapes = require('./lib/shapes.js');
-const { writeFile } = require('fs/promises');
+// const { run } = require('jest-cli');
+// const { getFileInfo } = require('prettier');
+
+const runShapes = new Shapes;
+
+class CLI {
+    Initials() {
+        return inquirer  
+            .prompt([  // accepts 
+            {
+                type: 'input',  // 1) 3 characters *TODO(confirm there are 3 character)
+                name: 'initials',
+                message: 'Input your initials.',
+            },
+            ]).then(({ initials }) => {
+                if(initials.length > 3) {
+                    console.log('Input must be 3 Characters or less.')
+                    return this.Initials();
+                } else {
+                    runShapes.text = initials;
+                    return this.getInfo();
+                }})
+            };
+
+    getInfo() {
+        return inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'textColor',  // 2) text color (keyword or hex value) *TODO(handle text or hex?)
+                    message: 'Enter a text color.',
+                },
+                {
+                    type: 'list',
+                    name: 'shape',  // 3) prompt shape - list: circle, triangle, square  *TODO(when to if-else?)
+                    message: 'Choose a shape.',
+                    choices: ['Circle', 'Triangle', 'Square'],
+                },
+                {
+                    type: 'input',
+                    name: 'shapeColor',     // 4) shapes color (keyword or hex value)
+                    message: 'Choose a background color.',
+                },
+            ]).then( ({ textColor, shape, shapeColor} ) => {
+                runShapes.textColor = textColor;
+                runShapes.shape = shape;
+                runShapes.shapeColor = shapeColor
+                runShapes.createLogo(); 
+            })};
+};
+
+const initiate = new CLI;
+initiate.Initials(); 
 
 
-
-inquirer  // CLI
-.prompt([  // accepts 
-{
-    type: 'input',  // 1) 3 characters *TODO(confirm there are 3 character)
-    name: 'initials',
-    message: 'Input your initials.',
-},
-{
-    type: 'input',
-    name: 'textColor',  // 2) text color (keyword or hex value) *TODO(handle text or hex?)
-    message: 'Enter a text color.',
-},
-{
-    type: 'list',
-    name: 'shape',  // 3) prompt shape - list: circle, triangle, square  *TODO(when to if-else?)
-    message: 'Choose a shape.',
-    choices: ['Circle', 'Triangle', 'Square'],
-},
-{
-    type: 'input',
-    name: 'shapeColor',     // 4) shapes color (keyword or hex value)
-    message: 'Choose a background color.',
-},
-]).then( ({ initials, textColor, shape, shapeColor} ) => {
-    this.initials = initials;
-    this.textColor = textColor;
-    this.shape = shape;
-    this.shapeColor = shapeColor
-    runShapes(initials, textColor, shape, shapeColor);
-    
-    createLogo(); 
-});
-const runShapes = new Shapes();
-
-const createLogo = () => {
-    return writeFile('./examples', userLogo, (err) =>
-    err ? console.log(err) : console.log('Successfully created logo.svg!')
-  );
-  };
 // 5) after receiving prompts generate logo.svg *TODO(when to writefile) ADD below
 // 6) output text console.log(Generated logo.svg) 
 // 7) open logo.svg into browser 300x200 pixel image
